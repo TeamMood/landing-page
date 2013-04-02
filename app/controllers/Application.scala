5e1ecf1b2fe6dcda7781e174a6f44182a61a5b0a
Nicolas Deverge <ndeverge@ekito.fr>
2013-04-02 13:42:43 +0200

    i18n

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,17 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.i18n.Lang
+import play.api.i18n.MessagesPlugin
+import play.api.i18n.Messages
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index())
+  def index = Action { request =>
+    {
+      // FIXME : there could be a better way to manage i18n
+      Ok(views.html.index(Lang.preferred(request.acceptLanguages)(Play.current)))
+    }
   }
 
 }
