2447d4f427dd9cdc4826d819d322205d4125108c
Nicolas Deverge <ndeverge@ekito.fr>
2013-04-04 17:18:47 +0200

    Autoping

--- project/Build.scala
+++ project/Build.scala
@@ -9,11 +9,13 @@ object ApplicationBuild extends Build {
 
   val appDependencies = Seq(
     // Add your project dependencies here,
+    "com.github.ndeverge" %% "autoping-play2-plugin" % "0.1.0-SNAPSHOT"
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here   
+     resolvers += Resolver.url("Autoping repository", url("http://ndeverge.github.com/autoping-play2-plugin/snapshots/"))(Resolver.ivyStylePatterns)
+
   )
 
 }
