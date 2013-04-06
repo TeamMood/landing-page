128048c9b8ea7fc9e1e499000863dc1feb95061b
Nicolas Deverge <ndeverge@ekito.fr>
2013-04-06 14:50:57 +0200

    Record ip, locale and browser infos

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,6 +7,7 @@ import play.api.i18n.MessagesPlugin
 import play.api.i18n.Messages
 import play.api.data.Forms._
 import play.api.data._
+import play.api.libs.ws.WS
 
 object Application extends Controller {
 
@@ -20,15 +21,29 @@ object Application extends Controller {
   def signup = Action {
     implicit request =>
       {
-        val emailForm = Form(
-          "input-email" -> text)
+        val emailForm = Form("input-email" -> text)
         val email = emailForm.bindFromRequest.get
 
-        //url: 'https://docs.google.com/forms/d/1vChMxuVCzNCLqPTusirUwrnAdRzBCFvVcmqqUxa5zuA/formResponse',
-        //entry.1
+        val ip = request.headers.get("x-forwarded-for").getOrElse(request.remoteAddress)
+        val locale = request.headers.get("Accept-Language")
+        val browser = request.headers.get("User-Agent")
+
+        save(email, ip, locale, browser)
 
         Ok(email)
       }
+  }
+
+  private def save(email: String, ip: String, locale: Option[String], browser: Option[String]) = {
+
+    val infos = Map(
+      "entry.1" -> Seq(email),
+      "entry.1924653994" -> Seq(ip),
+      "entry.535205848" -> Seq(locale.get),
+      "entry.192311241" -> Seq(browser.get))
+
+    // record infos to Google Doc asynchronously
+    WS.url("https://docs.google.com/forms/d/1vChMxuVCzNCLqPTusirUwrnAdRzBCFvVcmqqUxa5zuA/formResponse").post(infos)
 
   }
 
