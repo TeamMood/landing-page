6a6ab5a40c43254102df123281e72ffa8d28478f
Nicolas Deverge <ndeverge@ekito.fr>
2014-04-26 15:11:48 +0200

    Save to mailchimp

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,43 +3,73 @@ package controllers
 import play.api._
 import play.api.mvc._
 import play.api.i18n.Lang
-import play.api.i18n.MessagesPlugin
-import play.api.i18n.Messages
 import play.api.data.Forms._
 import play.api.data.validation.Constraints._
 import play.api.data._
 import play.api.libs.ws.WS
+import play.api.libs.json.{Json, JsValue}
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Application extends Controller {
 
-  def index = Action { request =>
-    {
+  def index = Action {
+    request => {
       // FIXME : there could be a better way to manage i18n
       Ok(views.html.index(Lang.preferred(request.acceptLanguages)(Play.current)))
     }
   }
 
+
   def signup = Action {
-    implicit request =>
-      {
-        val emailForm = Form("input-email" -> email.verifying(nonEmpty))
-        emailForm.bindFromRequest.fold(
-          errors => Redirect(routes.Application.index),
-          email => {
+    implicit request => {
+      val emailForm = Form("input-email" -> email.verifying(nonEmpty))
+      emailForm.bindFromRequest.fold(
+        errors => Redirect(routes.Application.index),
+        email => {
 
-            val ip = request.headers.get("x-forwarded-for").getOrElse(request.remoteAddress)
-            val locale = request.headers.get("Accept-Language")
-            val browser = request.headers.get("User-Agent")
+          val ip = request.headers.get("x-forwarded-for").getOrElse(request.remoteAddress)
+          val locale = request.headers.get("Accept-Language")
+          val browser = request.headers.get("User-Agent")
 
-            save(email, ip, locale, browser)
+          saveToGoogleDoc(email, ip, locale, browser)
 
-            Ok(views.html.thankyou(Lang.preferred(request.acceptLanguages)(Play.current)))
-          })
+          saveToMailchimp(email, ip, locale)
 
-      }
+          Ok(views.html.thankyou(Lang.preferred(request.acceptLanguages)(Play.current)))
+        })
+
+    }
   }
 
-  private def save(email: String, ip: String, locale: Option[String], browser: Option[String]) = {
+  private def saveToMailchimp(email: String, ip: String, locale: Option[String]) = {
+
+    var url = "https://us4.api.mailchimp.com/2.0//lists/subscribe.json"
+    var apiKey = "6bf455be4daddb948b4c1a494a9f6f47-us4"
+    var listId = "f0513fb469"
+
+    val json: JsValue = Json.obj(
+      "apikey" -> apiKey,
+      "id" -> listId,
+      "email" -> Json.obj(
+        "email" -> email
+      ),
+      "merge_vars" -> Json.obj(
+        "optin_ip" -> ip,
+        "mc_language" -> locale.get.take(2)
+      ),
+      "double_optin" -> false,
+      "send_welcome" -> false
+    )
+
+    WS.url(url).post(json).map {
+      response => println("status = " + response.status)
+        println("body = " + response.body)
+    }
+
+  }
+
+
+  private def saveToGoogleDoc(email: String, ip: String, locale: Option[String], browser: Option[String]) = {
 
     val infos = Map(
       "entry.1" -> Seq(email),
