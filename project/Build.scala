bf80d88cf6f3ebde4fad2554c8350710f1d9e37d
Nicolas Deverge <ndeverge@ekito.fr>
2015-06-08 18:17:38 +0200

    Java 6.

--- project/Build.scala
+++ project/Build.scala
@@ -16,9 +16,9 @@ object ApplicationBuild extends Build {
   val main = play.Project(appName, appVersion, appDependencies).settings(
      resolvers += Resolver.url("Autoping repository", url("http://ndeverge.github.com/autoping-play2-plugin/snapshots/"))(Resolver.ivyStylePatterns),
 
-       javacOptions ++= Seq("-source", "1.7", "-target", "1.7"),
+       javacOptions ++= Seq("-source", "1.6", "-target", "1.6"),
 
-       scalacOptions += "-target:jvm-1.7"
+       scalacOptions += "-target:jvm-1.6"
   )
 
 }
