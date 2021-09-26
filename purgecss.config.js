0856c2901719af0b488ac22c9e2763e8e88875b6
Youen Chéné <youen.chene@gadz.org>
2021-09-26 09:53:39 +0200

    Webvert - Purge CSS & Defer JS

---
+++ purgecss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // These are the files that Purgecss will search through
+    content: ["./_site/**/*.html"],
+  
+    // These are the stylesheets that will be subjected to the purge
+    css: ["./_site/css/style-mini.css","./_site/css/bootstrap.min.css","./_site/css/fontawesome.css"]
+  };
