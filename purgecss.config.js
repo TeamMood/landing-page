d821de20f9529be91537892e10631d912b13d073
Nicolas Deverge <nicolas@teammood.com>
2022-08-04 11:19:11 +0200

    simplifying purgecss

--- purgecss.config.js
+++ purgecss.config.js
@@ -3,5 +3,5 @@ module.exports = {
     content: ["./_site/**/*.html","./_site/js/*.js"],
   
     // These are the stylesheets that will be subjected to the purge
-    css: ["./_site/css/style-mini.css","./_site/css/bootstrap.min.css","./_site/css/fontawesome.css"]
+    css: ["./_site/css/style.css","./_site/css/bootstrap.min.css","./_site/css/fontawesome.css"]
   };
